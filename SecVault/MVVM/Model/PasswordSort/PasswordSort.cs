ae7d815abee774b71fed31051b2bee3830a64245
Shade <Shade@Shade.ga>
2024-02-06 18:44:22 +0400

    Completed Selected Type title and Sorting combo box

---
+++ SecVault/MVVM/Model/PasswordSort/PasswordSort.cs
@@ -0,0 +1,8 @@
+namespace SecVault.MVVM.Model.PasswordSort;
+
+public class PasswordSort
+{
+    public string? Name { get; set; }
+    
+    public PasswordSortType CategoryType { get; set; }
+}
---
+++ SecVault/MVVM/Model/PasswordSort/PasswordSortType.cs
@@ -0,0 +1,11 @@
+namespace SecVault.MVVM.Model.PasswordSort;
+
+public enum PasswordSortType
+{
+    Name,
+    Username,
+    Email,
+    Favourite,
+    CreatedDate,
+    LastUpdatedDate
+}
