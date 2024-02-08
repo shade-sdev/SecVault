7527ebebbf5374c7e2aa5f134a951fda3ea6df1a
SAUL <lavish.saulick@elca.mu>
2024-02-08 12:36:19 +0400

    Entity Framework

---
+++ SecVault/Data/PasswordContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using SecVault.MVVM.Model.Password;
+
+namespace SecVault.Data;
+
+public class PasswordContext : DbContext
+{
+    private readonly string _path = Environment.CurrentDirectory + @"\Password.db";
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlite($"Data Source={_path}");
+    
+    public DbSet<Password>? Passwords { get; set; }
+}
