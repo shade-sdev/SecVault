bf5952fa248b09aa26301447425e502d13b6b196
Shade <Shade@Shade.ga>
2024-02-09 21:00:50 +0400

    Handling multiple validation

--- SecVault/MVVM/View/Form/PasswordFormView.xaml.cs
+++ SecVault/MVVM/View/Form/PasswordFormView.xaml.cs
@@ -1,15 +1,14 @@
-using System.Windows;
 using System.Windows.Input;
 
 namespace SecVault.MVVM.View.Form;
 
-public partial class PasswordFormView : Window
+public partial class PasswordFormView
 {
     public PasswordFormView()
     {
         InitializeComponent();
     }
-    
+
     private void OnWindow_Drag(object sender, MouseEventArgs e)
     {
         if (e.LeftButton == MouseButtonState.Pressed)
