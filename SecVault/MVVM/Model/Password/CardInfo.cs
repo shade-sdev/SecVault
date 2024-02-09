7ea6d508d94d9b4cd261c498c46cf2673a3d31c6
Shade <Shade@Shade.ga>
2024-02-10 00:12:50 +0400

    formatting

--- SecVault/MVVM/Model/Password/CardInfo.cs
+++ SecVault/MVVM/Model/Password/CardInfo.cs
@@ -5,11 +5,15 @@ namespace SecVault.MVVM.Model.Password;
 [ComplexType]
 public class CardInfo
 {
-    [Column("card_number")] public string? CardNumber { get; set; }
+    [Column("card_number")] 
+    public string? CardNumber { get; set; }
 
-    [Column("card_pin")] public string? Pin { get; set; }
+    [Column("card_pin")] 
+    public string? Pin { get; set; }
 
-    [Column("card_notes")] public string? CardNote { get; set; }
+    [Column("card_notes")] 
+    public string? CardNote { get; set; }
 
-    [Column("card_expiry_date")] public DateTime? CardExpiryDate { get; set; }
+    [Column("card_expiry_date")] 
+    public DateTime? CardExpiryDate { get; set; }
 }
--- SecVault/MVVM/Model/Password/PasswordTypeInfo.cs
+++ SecVault/MVVM/Model/Password/PasswordTypeInfo.cs
@@ -7,11 +7,17 @@ namespace SecVault.MVVM.Model.Password;
 [ComplexType]
 public class PasswordTypeInfo
 {
-    [Required] [Column("name")] public string? Name { get; set; }
+    [Required] 
+    [Column("name")] 
+    public string? Name { get; set; }
 
-    [Url] [Column("website_url")] public string? WebsiteSiteUrl { get; set; }
+    [Url] 
+    [Column("website_url")] 
+    public string? WebsiteSiteUrl { get; set; }
 
-    [Url] [Column("icon_url")] public string? IconUrl { get; set; }
+    [Url] 
+    [Column("icon_url")] 
+    public string? IconUrl { get; set; }
 
     [Column("password_type")]
     public PasswordCategoryType PasswordCategoryType { get; set; } = PasswordCategoryType.Password;
