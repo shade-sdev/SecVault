171456f332ad37063fe4ddcd36780dbb4c63f7f7
Shade <Shade@Shade.ga>
2024-08-02 10:17:18 +0400

    Main.kt & App.kt

--- src/main/kotlin/ui/App.kt
+++ src/main/kotlin/ui/App.kt
@@ -1,12 +1,14 @@
 package ui
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 @Composable
@@ -17,12 +19,15 @@ fun App() {
     MaterialTheme {
         Column(
             modifier = Modifier.fillMaxSize()
-                .background(color = MaterialTheme.colorScheme.background)
+                .background(color = MaterialTheme.colorScheme.secondaryContainer),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
         )
         {
             Button(onClick = {
                 text = "Hello, Desktop!"
-            }) {
+            })
+            {
                 Text(text)
             }
         }
