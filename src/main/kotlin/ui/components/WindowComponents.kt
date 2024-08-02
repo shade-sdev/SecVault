7b5706ada59d4b54277590d4440a49305ea78d9e
Shade <Shade@Shade.ga>
2024-08-02 23:40:20 +0400

    Register Screen

--- src/main/kotlin/ui/components/WindowComponents.kt
+++ src/main/kotlin/ui/components/WindowComponents.kt
@@ -1,5 +1,8 @@
 package ui.components
 
+import androidx.compose.foundation.hoverable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsHoveredAsState
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -9,6 +12,8 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -17,12 +22,16 @@ import kotlin.system.exitProcess
 
 @Composable
 fun CloseButton() {
+    val interactionSource = remember { MutableInteractionSource() }
+    val isHovered by interactionSource.collectIsHoveredAsState()
+
     Button(
         onClick = { exitProcess(0) },
-        modifier = Modifier.size(30.dp),
+        modifier = Modifier.size(30.dp)
+            .hoverable(interactionSource),
         shape = RoundedCornerShape(topEnd = 10.dp),
         colors = ButtonColors(
-            containerColor = Color(0xFFb91919),
+            containerColor = if (isHovered) Color(0xFFb91919) else secondary,
             contentColor = Color.White,
             disabledContentColor = secondary,
             disabledContainerColor = secondary
