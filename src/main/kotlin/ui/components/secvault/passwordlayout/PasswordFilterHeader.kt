05d07e9198e01acb1c66c4ebe01fb5bcaa84adea
Shade <Shade@Shade.ga>
2024-08-16 15:59:02 +0400

    Shimmer loading

--- src/main/kotlin/ui/components/secvault/passwordlayout/PasswordFilterHeader.kt
+++ src/main/kotlin/ui/components/secvault/passwordlayout/PasswordFilterHeader.kt
@@ -67,7 +67,6 @@ fun PasswordFilterHeader(screenModel: SecVaultScreenModel) {
                 )
                 {
                     Icon(Icons.Outlined.Add, contentDescription = "Localized description")
-
                 }
             }
         }
