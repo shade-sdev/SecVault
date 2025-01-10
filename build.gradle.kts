2f6f8c8cbf3870c508b532ae9a5845ac0d6f7711
SAUL <lavish.saulick@elca.mu>
2025-01-10 14:37:52 +0400

    Code documentation

--- build.gradle.kts
+++ build.gradle.kts
@@ -84,11 +84,3 @@ compose.desktop {
         }
     }
 }
-
-allprojects {
-    gradle.projectsEvaluated {
-        tasks.withType<JavaCompile> {
-            options.compilerArgs.add("-Xlint:-deprecation")
-        }
-    }
-}
